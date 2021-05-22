e55b88ef5ef3866817769ea2ae5e16cb3f62763c
abhinitro <pandeyabhilash809@gmail.com>
2021-05-23 01:01:19 +0530

    added the tree view

--- frontend/src/views/dashboard/menu/TableTree.vue
+++ frontend/src/views/dashboard/menu/TableTree.vue
@@ -19,151 +19,18 @@
 
     <!-- table -->
     <vue-good-table
-      mode="remote"
-      :columns="columns"
-      :rows="rows"
-      :rtl="direction"
-      :search-options="{
-        enabled: true,
-        externalQuery: searchTerm
-      }"
-      :select-options="{
-        enabled: true,
-        selectOnCheckboxOnly: true,
-        selectionInfoClass: 'custom-class',
-        selectionText: 'rows selected',
-        clearSelectionText: 'clear',
-        disableSelectInfo: true,
-        selectAllByGroup: true,
-      }"
-      :pagination-options="{
-        enabled: true,
-      }"
-      @on-sort-change="onSortChange"
+     
+    :columns="columns"
+    :rows="rows"
+    :group-options="{
+  	  enabled: true,
+      collapsable: true
+    }"
     >
-      <template
-        slot="table-row"
-        slot-scope="props"
-      >
-
-        <!-- Column: Name -->
-        <span
-          v-if="props.column.field === 'fullName'"
-          class="text-nowrap"
-        >
-          <b-avatar
-            :src="props.row.avatar"
-            class="mx-1"
-          />
-          <span class="text-nowrap">{{ props.row.fullName }}</span>
-        </span>
-
-        <!-- Column: Status -->
-        <span v-else-if="props.column.field === 'status'">
-          <b-badge :variant="statusVariant(props.row.status)">
-            {{ props.row.status }}
-          </b-badge>
-        </span>
-
-        <!-- Column: Action -->
-        <span v-else-if="props.column.field === 'action'">
-          <span>
-            <b-dropdown
-              variant="link"
-              toggle-class="text-decoration-none"
-              no-caret
-            >
-              <template v-slot:button-content>
-                <feather-icon
-                  icon="MoreVerticalIcon"
-                  size="16"
-                  class="text-body align-middle mr-25"
-                />
-              </template>
-              <b-dropdown-item>
-                <feather-icon
-                  icon="Edit2Icon"
-                  class="mr-50"
-                />
-                <span>Edit</span>
-              </b-dropdown-item>
-              <b-dropdown-item>
-                <feather-icon
-                  icon="TrashIcon"
-                  class="mr-50"
-                />
-                <span>Delete</span>
-              </b-dropdown-item>
-            </b-dropdown>
-          </span>
-        </span>
-
-        <!-- Column: Common -->
-        <span v-else>
-          {{ props.formattedRow[props.column.field] }}
-        </span>
-      </template>
-
-      <!-- pagination -->
-      <template
-        slot="pagination-bottom"
-        slot-scope="props"
-      >
-        <div class="d-flex justify-content-between flex-wrap">
-
-          <!-- page length -->
-          <div class="d-flex align-items-center mb-0 mt-1">
-            <span class="text-nowrap ">
-              Showing 1 to
-            </span>
-            <b-form-select
-              v-model="pageLength"
-              :options="pages"
-              class="mx-1"
-              @input="handlePageChange"
-            />
-            <span class="text-nowrap"> of {{ props.total }} entries </span>
-          </div>
-
-          <!-- pagination -->
-          <div>
-            <b-pagination
-              :value="1"
-              :total-rows="props.total"
-              :per-page="pageLength"
-              first-number
-              last-number
-              align="right"
-              prev-class="prev-item"
-              next-class="next-item"
-              class="mt-1 mb-0"
-              @change="handleChangePage"
-            >
-              <template #prev-text>
-                <feather-icon
-                  icon="ChevronLeftIcon"
-                  size="18"
-                />
-              </template>
-              <template #next-text>
-                <feather-icon
-                  icon="ChevronRightIcon"
-                  size="18"
-                />
-              </template>
-            </b-pagination>
-          </div>
-        </div>
-      </template>
+      
     </vue-good-table>
 
-    <prism class="rounded mt-1">
-      {{ log.join("\n") }}
-    </prism>
-
-    <template #code>
-      {{ codeSSR }}
-    </template>
+    
   </b-card-code>
 </template>
 
@@ -203,30 +70,30 @@ export default {
       columns: [
         {
           label: 'Name',
-          field: 'fullName',
+          field: 'name',
         },
         {
-          label: 'Email',
-          field: 'email',
+          label: 'Age',
+          field: 'age',
+          type: 'number',
         },
         {
-          label: 'Date',
-          field: 'startDate',
-        },
-        {
-          label: 'Salary',
-          field: 'salary',
-        },
-        {
-          label: 'Status',
-          field: 'status',
-        },
-        {
-          label: 'Action',
-          field: 'action',
+          label: 'Percent',
+          field: 'score',
+          type: 'percentage',
         },
       ],
-      rows: [],
+      rows: [
+        { id: 0, name: 'Senior Devs', score: 0.15296, children: [
+          { id: 1, name: 'John',  age: 20, score: 0.03845 },
+          { id: 2, name: 'Jane',  age: 24, score: 0.02948 },
+          { id: 3, name: 'Susan', age: 16, score: 0.08503 }
+        ]},
+        { id: 4, name: 'Junior Devs', score: 0.12069, children: [
+          { id: 5, name: 'Chris', age: 55, score: 0.02946 },
+          { id: 6, name: 'Dan',   age: 40, score: 0.09123 }
+        ]}
+      ],
       searchTerm: '',
       status: [{
         1: 'Current', 2: 'Professional', 3: 'Rejected', 4: 'Resigned', 5: 'Applied',
@@ -262,8 +129,8 @@ export default {
     },
   },
   created() {
-    this.$http.get('/good-table/table_ssr')
-      .then(res => { this.rows = res.data })
+    // this.$http.get('/good-table/table_ssr')
+    //   .then(res => { this.rows = [] })
   },
   methods: {
     handleSearch(searching) {
